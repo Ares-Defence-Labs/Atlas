7786f80097aab91128145d4ce70f195784d8be82
TheArchitect123 <dan.developer789@gmail.com>
2025-03-22 17:20:44 +1100

    v0.3.1 (ViewBinding Apis)

--- atlas-droid-mvvm-viewbinding/build.gradle.kts
+++ atlas-droid-mvvm-viewbinding/build.gradle.kts
@@ -62,62 +62,62 @@ dependencies {
     implementation(libs.androidx.app.lifecycle.viewmodel.runtime)
 }
 //
-//mavenPublishing {
-//    // Define coordinates for the published artifact
-//    coordinates(
-//        groupId = "io.github.thearchitect123",
-//        artifactId = "atlas-mvvm-view-binding",
-//        version = "0.3.0"
-//    )
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.thearchitect123",
+        artifactId = "atlas-mvvm-view-binding",
+        version = "0.3.1"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("AtlasMvvmViewBinding")
+        description.set("Atlas View Binding implementation library for atlas-core")
+        inceptionYear.set("2025")
+        url.set("https://github.com/TheArchitect123/Atlas")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("Dan Gerchcovich")
+                name.set("TheArchitect123")
+                email.set("dan.developer789@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/TheArchitect123/Atlas")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
 //
-//    // Configure POM metadata for the published artifact
-//    pom {
-//        name.set("AtlasMvvmViewBinding")
-//        description.set("Atlas View Binding implementation library for atlas-core")
-//        inceptionYear.set("2025")
-//        url.set("https://github.com/TheArchitect123/Atlas")
-//
-//        licenses {
-//            license {
-//                name.set("MIT")
-//                url.set("https://opensource.org/licenses/MIT")
-//            }
-//        }
-//
-//        // Specify developers information
-//        developers {
-//            developer {
-//                id.set("Dan Gerchcovich")
-//                name.set("TheArchitect123")
-//                email.set("dan.developer789@gmail.com")
-//            }
-//        }
-//
-//        // Specify SCM information
-//        scm {
-//            url.set("https://github.com/TheArchitect123/Atlas")
-//        }
-//    }
-//
-//    // Configure publishing to Maven Central
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-//
-//    // Enable GPG signing for all publications
-//    signAllPublications()
-//}
-////
-//signing {
-//    val privateKeyFile = project.findProperty("signing.privateKeyFile") as? String
-//        ?: error("No Private key file found")
-//    val passphrase = project.findProperty("signing.password") as? String
-//        ?: error("No Passphrase found for signing")
-//
-//    // Read the private key from the file
-//    val privateKey = File(privateKeyFile).readText(Charsets.UTF_8)
-//
-//    useInMemoryPgpKeys(privateKey, passphrase)
-//    sign(publishing.publications)
-//}
+signing {
+    val privateKeyFile = project.findProperty("signing.privateKeyFile") as? String
+        ?: error("No Private key file found")
+    val passphrase = project.findProperty("signing.password") as? String
+        ?: error("No Passphrase found for signing")
+
+    // Read the private key from the file
+    val privateKey = File(privateKeyFile).readText(Charsets.UTF_8)
+
+    useInMemoryPgpKeys(privateKey, passphrase)
+    sign(publishing.publications)
+}
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     outputs.upToDateWhen { false } // ✅ Always recompile Kotlin sources
